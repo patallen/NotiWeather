d15c97193106668f03f6a34859a054341686a9cb
Patrick Allen <prallen90@gmail.com>
2015-09-28 21:24:37 -0400

    Add crispy forms and main template

--- NotiWeather/settings.py
+++ NotiWeather/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'newsletter'
+
+    'crispy_forms',
+    'newsletter',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -101,3 +103,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
