7f403f667c2408bb35bb3e129a9a0d029dded9d0
Patrick Allen <prallen90@gmail.com>
2015-09-28 22:48:30 -0400

    Add success page for post-subscribe

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -1,6 +1,8 @@
 from newsletter.views import SubscribeView
+from django.views.generic.base import TemplateView
 from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', SubscribeView.as_view(), name='subscribe'),
+    url(r'^success/', TemplateView.as_view(template_name='success.html'), name='subscribe'),
 ]
