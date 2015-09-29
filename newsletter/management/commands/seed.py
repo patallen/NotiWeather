37744a08379c13aaf00fb8b634c5a57c690691ac
Patrick Allen <prallen90@gmail.com>
2015-09-29 16:51:24 +0000

    Clean up seed management command

--- newsletter/management/commands/seed.py
+++ newsletter/management/commands/seed.py
@@ -9,17 +9,18 @@ csv_path = directory +  '/topcities.csv'
 
 class Command(BaseCommand):
     """
-    Custom Django manage.py command that seeds the database with
-    the top 100 cities by population.
-    $ python manage.py seed
+    Custom Django manage.py command that seeds the database with the top 100
+    cities by population.
+
+    Usage: `$ python manage.py seed`
     """
     def handle(self, *args, **options):
-        print(csv_path)
         with open(csv_path, 'r', encoding='mac_roman') as csvfile:
             cityreader = csv.reader(csvfile, delimiter=',')
             for city in cityreader:
                 try:
-                    c, created = Location.objects.get_or_create(city=city[0], state_long=city[1], state_short=city[2])
-                    print(created)
+                    Location.objects.get_or_create(city=city[0],
+                                                   state_long=city[1],
+                                                   state_short=city[2])
                 except:
                     pass
